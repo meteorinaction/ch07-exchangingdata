81ebe2788df517be09c611af85aaa69291a9e8d7
Stephan Hochhaus <stephan@yauh.de>
2015-04-25 16:47:49 +0200

    actually show aggregated data to the user

--- workoutTracker/client/workoutTracker.js
+++ workoutTracker/client/workoutTracker.js
@@ -42,3 +42,31 @@ Template.workoutList.events({
 
   }
 });
+
+monthLabels = {
+  1: 'January',
+  2: 'February',
+  3: 'March',
+  4: 'April',
+  5: 'Mai',
+  6: 'June',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December'
+};
+
+Template.distanceByMonth.helpers({
+  month: function () {
+    return DistanceByMonth.find({}, {
+      sort: {
+        _id: 1
+      }
+    });
+  },
+  translateMonth: function () {
+    return monthLabels[this._id];
+  }
+});
