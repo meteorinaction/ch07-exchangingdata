5521875d99de42543f1ecca36d7ccc4bd2003e15
Stephan Hochhaus <stephan@yauh.de>
2015-04-25 16:20:51 +0200

    Listing 7.6 Aggregation inside a publication

--- workoutTracker/server/publications.js
+++ workoutTracker/server/publications.js
@@ -14,3 +14,34 @@ Meteor.publish('workouts', function (options) {
 
   return WorkoutsCollection.find(qry, qryOptions);
 });
+
+Meteor.publish('distanceByMonth', function () {
+  var subscription = this;
+  var db = MongoInternals.defaultRemoteCollectionDriver().mongo.db;
+  var pipeline = [{
+    $group: {
+      _id: {
+        $month: '$workoutAt'
+      },
+      distance: {
+        $sum: '$distance'
+      }
+    }
+  }];
+
+  db.collection('workouts').aggregate(
+    pipeline,
+    Meteor.bindEnvironment(
+      function (err, result) {
+        console.log('result', result);
+        _.each(result, function (r) {
+          subscription.added('distanceByMonth', r._id, {
+            distance: r.distance
+          });
+        })
+      }
+    )
+  )
+
+  subscription.ready();
+});
